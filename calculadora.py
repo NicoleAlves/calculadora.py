5dad3e37cd822048b06cfdf17b35e786fa84a2ba
NicoleAlves <nicolealvesff@poli.ufrj.br>
2020-12-12 22:15:28 -0300

    Criando a calculadora
    Início, matemática básica

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+def soma(x,y):
+  return x+y
+
+def sub(x,y):
+  return x-y
+
+def mult(x,y):
+  return x*y
+
+def div(x,y):
+  return x/y
+  
+def elev(x,y):
+  return x**y
+  
+def raiz(x,y):
+  return x**(1/y)
+  
